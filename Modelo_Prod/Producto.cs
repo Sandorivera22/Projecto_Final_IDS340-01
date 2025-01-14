3468f331b8f1d432f6646b2b9a43a190fdb179ee
Sandorivera22 <147453328+Sandorivera22@users.noreply.github.com>
2025-01-14 08:49:19 -0400

    Se agrega el Proyecto

---
+++ Modelo_Prod/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  WindowsFormsProductos.Modelo_Prod
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string categoria { get; set; }
+        public double precio { get; set; }
+        public int stock { get; set; }
+        public string proveedor { get; set; }
+    }
+}
---
+++ Modelo_Prov/Proveedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  WindowsFormsProveedores.Modelo_Prov
+{
+    public class Proveedor
+    {
+        public int id { get; set; }
+        public string nombre_empresa { get; set; }
+        public string contacto { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+    }
+}
